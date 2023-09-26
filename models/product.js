c787da0699a056d272eb014582463d623cc74755
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-09-26 11:50:11 +0530

    deleting product

--- models/product.js
+++ models/product.js
@@ -3,20 +3,23 @@ const mongodb = require("mongodb");
 
 class Product {
   constructor(title, price, description, imageUrl, id) {
+    console.log(">>>>>>>id to update", typeof id);
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = id;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
+    console.log(">>>>>>>id to update22222", this);
+
     const db = getDb();
     let dbOp;
     if (this._id) {
       dbOp = db
         .collection("products")
-        .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+        .updateOne({ _id: this._id }, { $set: this });
     } else {
       dbOp = db.collection("products").insertOne(this);
     }
@@ -53,6 +56,15 @@ class Product {
         console.log(">>>>>>>>>>>>>>>>", data);
         return data;
       });
+  }
+
+  static deleteById(prodId) {
+    const db=getDb()
+    return db.collection("products").deleteOne({ _id: new mongodb.ObjectId(prodId)}).then(result=>{
+      console.log("deleted")
+    }).catch(err=>[
+      console.log(err)
+    ])
   }
 }
 
