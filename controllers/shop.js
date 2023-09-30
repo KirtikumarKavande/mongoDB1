2dbe3860144959c1852b637936e6c4f41195ec7c
KirtikumarKavande <kirtikumar0005@gmail.com>
2023-09-30 10:13:49 +0530

    adding Item to order from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -70,20 +70,16 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then((result) => {
-
       console.log(result);
-      res.redirect('/cart')
+      res.redirect("/cart");
     });
-
-
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
     .deleteItemFromCart(prodId)
-    
-    
+
     .then((result) => {
       res.redirect("/cart");
     })
@@ -93,30 +89,11 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   let fetchedCart;
   req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      return req.user
-        .createOrder()
-        .then((order) => {
-          return order.addProducts(
-            products.map((product) => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch((err) => console.log(err));
-    })
-    .then((result) => {
-      return fetchedCart.setProducts(null);
-    })
+    .postOrder()
     .then((result) => {
       res.redirect("/orders");
     })
+
     .catch((err) => console.log(err));
 };
 
